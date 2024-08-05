f9af71a0cf4a79bb1b8f929c9fa3a0a52344ee16
Pritikana Mondal <167571104+priti275@users.noreply.github.com>
2024-08-05 23:40:20 +0530

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,10 @@
+const product = {
+    fullName : "bluepen",
+    rating: 7.9,
+    offer : 67,
+    price: 342,
+
+    
+};
+console.log(product["price"])
+
---
+++ script.js
@@ -0,0 +1,5 @@
+let a = 7;
+let b = 9;
+let cond1 = a<b;
+let cond2 = b>a;
+console.log("cond1 && cond2" ,cond1 && cond2);
